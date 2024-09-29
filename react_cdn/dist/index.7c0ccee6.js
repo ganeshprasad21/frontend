84d73e1aa6197f5199d8445f8f72ce802856bd49
Ganesh Prasad R <ganesh21111999@gmail.com>
2024-09-29 21:05:48 +0530

    added react inside project

---
+++ react_cdn/dist/index.7c0ccee6.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h1", {
+    id: "ganesh"
+}, "<3");
+const heading123 = React.createElement("h1", {
+    id: "ganeshPrasadR"
+}, "<3<3<3");
+const bundle = React.createElement("div", {
+    id: "container"
+}, [
+    heading1,
+    heading123
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(bundle);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
